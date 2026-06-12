4f3189fcebe8918de11f2da80cfc24cdfa50602d
ssuuji <sub9399@naver.com>
2026-06-12 16:49:18 +0900

    initial commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static Vector2 Movement { get; private set; } = Vector2.zero;
+    public static bool IsJump { get; private set; }
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    private void Awake()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+    }
+
+    private void Update()
+    {
+        Movement = moveAction.ReadValue<Vector2>();
+        IsJump = jumpAction.WasPressedThisFrame();
+    }
+}
