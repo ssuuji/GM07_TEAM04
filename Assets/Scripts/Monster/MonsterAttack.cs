cf4140f6bb21676780c7032849faeb158848e312
JuTaeHyung <142490305+JuTaeHyung@users.noreply.github.com>
2026-06-17 17:11:49 +0900

    fix : takeDamage to public

--- Assets/Scripts/Monster/MonsterAttack.cs
+++ Assets/Scripts/Monster/MonsterAttack.cs
@@ -2,15 +2,7 @@ using UnityEngine;
 
 public class MonsterAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private int attackPower = 2; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
