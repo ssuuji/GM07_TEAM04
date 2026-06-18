4ffee7b7794141acea41a7ea832ff2efa3733319
ssuuji <sub9399@naver.com>
2026-06-19 01:01:17 +0900

    feat : 플레이어 레벨 및 경험치 , 스킬해금 구현

---
+++ Assets/Scripts/Player/TestExp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TestExp : MonoBehaviour
+{
+    [SerializeField] private int expAmount = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerLevel level = collision.GetComponent<PlayerLevel>();
+
+        level.AddExp(expAmount);
+
+        Destroy(gameObject);
+    }
+
+    
+}
