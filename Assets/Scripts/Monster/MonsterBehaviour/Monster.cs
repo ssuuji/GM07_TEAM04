a3dffd1a8f86ba61ce4c6b2137df9f853b932d46
JuTaeHyung <142490305+JuTaeHyung@users.noreply.github.com>
2026-06-18 18:23:22 +0900

    feat : 몬스터 체력UI, 몬스터 넉백 추가

---
+++ Assets/Scripts/Monster/MonsterBehaviour/Monster.cs
@@ -0,0 +1,139 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour, IDamageable
+{
+    
+    enum MonsterState
+    {
+        None = -1, Idle, Angry, Knockback, Attack
+    }
+
+    private MonsterState currentState;
+
+    [Header("몬스터 상태")]
+    [SerializeField] private int maxHealth = 10;
+    [SerializeField] private float knockbackTime = 1.0f; 
+    [SerializeField] private MonsterIdleMove monsterIdleMove;
+    [SerializeField] private MonsterAngryMove monsterAngryMove;
+    [SerializeField] private MonsterUI monsterUI;
+    [SerializeField] private MonsterKnockBack monsterKnockBack;
+
+    private float flairTime = 3;
+    private float knockbackTimer;
+    public bool IsDead { get; private set; } = false;
+    public bool Direction { get; private set; } = true;
+    private int currentHealth;
+    
+    
+
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+        currentState = MonsterState.Idle;
+        SetKnockbackTime();
+    }
+
+    private void Start()
+    {
+        if (monsterUI != null)
+        {
+            monsterUI.Initialize(maxHealth);
+        }
+    }
+
+
+    private void Update()
+    {
+        //테스트용
+        Flair();
+        StateManage();
+        
+    }
+
+    //피해
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+
+        if (monsterUI!=null)
+        {
+            monsterUI.SetHP(currentHealth);
+        }
+        SetState(MonsterState.Knockback);
+        monsterKnockBack.Knockback();
+    }
+
+    //죽음
+    private void Die()
+    {
+        IsDead = true;
+        Destroy(gameObject);
+    }
+
+
+    //상태
+    private void SetState(MonsterState state)
+    {
+        currentState = state;
+
+        monsterIdleMove.enabled = state == MonsterState.Idle;
+        monsterAngryMove.enabled = state == MonsterState.Angry;
+        monsterKnockBack.enabled = state == MonsterState.Knockback;
+        //monsterAttack.enabled = state == MonsterState.Attack;
+    }
+
+
+
+    private void StateManage()
+    {
+        if (currentState == MonsterState.Knockback)
+        {
+            knockbackTimer -= Time.deltaTime;
+            if(knockbackTimer <= 0)
+            {
+                SetState(MonsterState.Angry);
+                SetKnockbackTime();
+            }
+            
+        }
+        else
+        {
+            if (currentHealth < maxHealth)
+            {
+                SetState(MonsterState.Angry);
+            }
+        }
+    }
+
+    private void SetKnockbackTime()
+    {
+        knockbackTimer = knockbackTime;
+    }
+
+
+    //방향설정
+    public void SetDirection(bool dir)
+    {
+        Direction = dir;
+    }
+
+    //테스트용
+    private void Flair()
+    {
+        if (flairTime <= 0)
+        {
+            TakeDamage(1);  
+            flairTime = 3;
+        }
+        else
+        {
+            flairTime -= Time.deltaTime;
+        }
+    }
+    
+}
