a866f47f11f7ba88c77819ef7d6ad30936cf5c0b
ssuuji <sub9399@naver.com>
2026-06-19 17:36:57 +0900

    Feat : TitleScene 및 GameScene 추가
    - Title Scene 버튼 및 씬 이동 구현
    - GameScene 에 플레이어 파트 추가

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -2,8 +2,23 @@
 
 public class PlayerInteraction : MonoBehaviour
 {
+    private bool isInShop; //상점 NPC 범위안인지 확인 여부
+    public bool IsInShop => isInShop;
     public void Interact()
     {
+        //상점NPC 범위라면
+        if (IsInShop)
+        {
+            Debug.Log("상점 오픈");
+            return;
+        }
+
         Debug.Log("상호작용");
+    }
+
+    //상점 NPC 범위 상태 변경
+    public void SetInShop(bool value)
+    {
+        isInShop = value;
     }
 }
