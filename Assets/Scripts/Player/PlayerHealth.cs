bfb3631da46073a0286e452d3d46de87c3ec242b
ssuuji <sub9399@naver.com>
2026-06-21 23:05:37 +0900

    feat : 씬전환 및 플레이어 넉백 구현

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
     private PlayerStatus playerStatus;
-    private bool isDead;  //플레이어 사망여부
-    private bool isInvin; //무적상태 확인여부
+    private bool isDead;       //플레이어 사망여부
+    private bool isInvin;      //무적상태 확인여부
+
+    private PlayerMovement playerMovement; 
+    private Rigidbody2D rb;
+    private float knockBackPower = 5.0f; //넉백 파워
+    private float knockBackTime = 0.15f; //넉백 유지시간
+    private bool isKnockBack;            //넉백 중인지 확인여부
+    private Coroutine currentCo;         //피격 색상 코루틴
+
+    public bool IsKnockBack => isKnockBack;
 
     private void Start()
     {
         playerStatus = GetComponent<PlayerStatus>();
+        playerMovement = GetComponent<PlayerMovement>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     //데미지 받을 때
@@ -25,13 +37,63 @@ public class PlayerHealth : MonoBehaviour, IDamageable
         {
             Die();
         }
+
+        //피격시 넉백 적용 및 색상처리(추후 색상 -> 애니메이션으로 변경예정)
+        Knockback();
+        DamageColor();
     }
+
+    private void Knockback()
+    {
+        StartCoroutine(KnockbackCo());
+    }
+
+    IEnumerator KnockbackCo()
+    {
+        isKnockBack = true;
+
+        //플레이어가 바라보는 방향의 반대방향으로 
+        float dir = -playerMovement.CheckDirValue;
+
+        //넉백 적용
+        rb.linearVelocity = new Vector2(dir * knockBackPower, rb.linearVelocity.y);
+
+        yield return new WaitForSeconds(knockBackTime);
+
+        isKnockBack = false;
+    }
+
+    private void DamageColor()
+    {
+        if (currentCo != null)
+        {
+            StopCoroutine(currentCo);
+        }
+        currentCo = StartCoroutine(ColorCo());
+    }
+
+    IEnumerator ColorCo()
+    {
+        Renderer renderer = GetComponentInChildren<Renderer>();
+        Color originColor = renderer.material.color;
+
+        renderer.material.color = Color.yellow;
+
+        yield return new WaitForSeconds(0.15f);
+
+        renderer.material.color = originColor;
+
+        currentCo = null;
+    }
+
 
     //플레이어 사망처리
     private void Die()
     {
         isDead = true;
         Debug.Log("플레이어 사망");
+
+        GameSceneManager.Instance.LoadScene(SceneType.GameOver);
     }
 
     //무적 상태 변경
