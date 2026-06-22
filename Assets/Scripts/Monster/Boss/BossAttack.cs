aa0bedb891c9a4c49c4af91364b2d2d933c02814
JuTaeHyung <142490305+JuTaeHyung@users.noreply.github.com>
2026-06-22 18:35:38 +0900

    feat : 보스 공격 구현 진행

--- Assets/Scripts/Monster/Boss/BossAttack.cs
+++ Assets/Scripts/Monster/Boss/BossAttack.cs
@@ -5,17 +5,40 @@ using UnityEngine;
 public class BossAttack : MonoBehaviour
 {
     [SerializeField] private Transform shootingPoint;
+    [SerializeField] private Transform droppingPoint;
+    [SerializeField] private float attackDuration = 5.0f;
+    [SerializeField] private float attackCoolTime = 1.0f;
     [SerializeField] private float shootCoolTime = 1.0f;
+    [SerializeField] private float dropCoolTime = 1.0f;
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private GameObject dropPrefab;
+
+    private int randomInt;
+    private Coroutine currentAttack;
+
+    
 
     private void Awake()
     {
         shootingPoint = transform.Find("ShootingPoint");
+        droppingPoint = transform.Find("DroppingPoint");
     }
 
     private void Start()
     {
-        StartCoroutine(ShootingCo());
+        StartCoroutine(AttackCo());
+    }
+
+    IEnumerator AttackCo()
+    {
+        while (true)
+        {
+            randomInt = Random.Range(0, 2);
+            StartAttack(randomInt);
+            yield return new WaitForSeconds(attackDuration);
+            EndAttack();
+            yield return new WaitForSeconds(attackCoolTime);
+        }
     }
 
     IEnumerator ShootingCo()
@@ -27,9 +50,46 @@ public class BossAttack : MonoBehaviour
         }
     }
 
+    IEnumerator DroppingCo()
+    {
+        while (true)
+        {
+            Drop();
+            yield return new WaitForSeconds(dropCoolTime);
+        }
+    }
+    
+    private void StartAttack(int random)
+    {
+        switch (random)
+        {
+            case 0: 
+                currentAttack = StartCoroutine(ShootingCo());
+                break;
+
+            case 1:
+                currentAttack = StartCoroutine(DroppingCo());
+                break;
+        }
+
+    }
+
+    private void EndAttack()
+    {
+        if(currentAttack != null)
+        {
+            StopCoroutine(currentAttack);
+            currentAttack = null;
+        }
+    }
 
     private void Shoot()
     {
-        Instantiate(bulletPrefab, shootingPoint.transform.position, Quaternion.identity);
+        Instantiate(bulletPrefab, shootingPoint.position, Quaternion.identity);
+    }
+
+    private void Drop()
+    {
+        Instantiate(dropPrefab, droppingPoint.position, Quaternion.identity);
     }
 }
--- Assets/Scripts/Monster/Boss/Drop/Drop.cs
+++ Assets/Scripts/Monster/Boss/Drop/Drop.cs
@@ -2,15 +2,63 @@ using UnityEngine;
 
 public class Drop : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private int damage = 10;
+    [SerializeField] private float dropSpeed = 5.0f;
+    [SerializeField] private float lifeTime = 5.0f;
+    [SerializeField] private PlayerHealth player;
+    private Rigidbody2D rb;
+    private float currentLifeTime;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        currentLifeTime = lifeTime;
+    }
+
+    private void Update()
+    {
+        Aging();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Aging()
+    {
+        currentLifeTime -= Time.deltaTime;
+        if(currentLifeTime <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = new Vector2(0, -dropSpeed);
+    }
+
+    private void Die()
+    {
+        rb.linearVelocity = Vector2.zero;
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Die();
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player.TakeDamage(damage);
+        }
     }
 }
