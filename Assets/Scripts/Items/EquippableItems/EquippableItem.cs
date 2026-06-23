b7985852dcbcf229e450829b3ce5bd4109d725a2
RyuCy <rcy1235@naver.com>
2026-06-23 17:54:36 +0900

    docs: Update Comments
     - 주석 수정

--- Assets/Scripts/Items/EquippableItems/EquippableItem.cs
+++ Assets/Scripts/Items/EquippableItems/EquippableItem.cs
@@ -4,6 +4,7 @@ public abstract class EquippableItem : Item
 {
     [Header("Equipment Settings")]
     [SerializeField] private EquipmentType equipType;
+    // 아무래도 강화 정보는 여기서 처리해야겠지?
 
     // 프로퍼티
     public EquipmentType EquipType => equipType;
@@ -17,25 +18,29 @@ public abstract class EquippableItem : Item
     /*=============== Method ===============*/
 
     // 장비 장착
+    // Equip/UnEquip을 이용해 조건에 맞게 아이템 장착/해제를 담당할 메서드
     public override void Use(GameObject target, int itemID)
     {
-        // Equip/UnEquip을 이용해 조건에 맞게 아이템 장착/해제를 담당할 메서드
         if (target == null) return;
         PlayerEquipment playerEquipment = target.GetComponent<PlayerEquipment>();
         if (playerEquipment == null) return;
-
+        // 현재 아이템의 장착 여부 확인
+        // 플레이어가 장비를 장착 중인지 + 장착된 아이템의 ID값과 이 아이템의 ID값이 같은지 확인
         bool isEquipped = (playerEquipment.WeaponItem != null && playerEquipment.WeaponItem.ItemID == itemID) ||
                           (playerEquipment.ArmorItem != null && playerEquipment.ArmorItem.ItemID == itemID);
-
+        // 장착 중인 아이템이라면
         if (isEquipped)
         {
-            // 이미 장착 중이면 해제
+            // 장착 해제
             this.UnEquip(target);
         }
+        // 장착 중인 아이템이 아니라면
         else
         {
+            // 장착 중인 아이템 확인
             if ((playerEquipment.IsWeapon) || (playerEquipment.IsArmor))
             {
+                // 장착 해제
                 this.UnEquip(target);
             }
             // 장착 중이 아니면 새로 장착
