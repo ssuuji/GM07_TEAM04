69b3e0da5a14a02f7a0fcb95c5465de8cda3674a
ssuuji <sub9399@naver.com>
2026-06-25 18:08:49 +0900

    feat : 상자 및 포탈 상호작용 구현

---
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    [Header("리스폰 위치")]
+    private Transform respawnPoint;
+
+    //리스폰 위치 변경
+    public void SetRespawnPoint(Transform point)
+    {
+        respawnPoint = point;
+    }
+
+    //리스폰
+    public void Respawn()
+    {
+        if (respawnPoint == null) return;
+
+        transform.position = respawnPoint.position;
+    }
+}
