339b465dde9364485a7248c245c042206763c187
RyuCy <rcy1235@naver.com>
2026-06-30 15:17:07 +0900

    feat: Add ConsumableItem Use CoolDown
     - 소모품 사용 쿨타임 추가. SO 파일에서 수정 가능

--- Assets/Scripts/UI/Player/ConsumableQuickSlotUI/ConsumableQuickSlotManager.cs
+++ Assets/Scripts/UI/Player/ConsumableQuickSlotUI/ConsumableQuickSlotManager.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ConsumableQuickSlotManager : Singleton<ConsumableQuickSlotManager>
 {
     // 1~3번 퀵슬롯에 저장된 소모품 데이터
     public ConsumableItem[] quickSlots = new ConsumableItem[3];
-    
+    // 슬롯별 쿨타임 상태 확인
+    private List<int> coolingDownItemIDs = new List<int>();
+
+    // 이벤트
     // UI 갱신을 위한 이벤트
     public event Action OnQuickSlotUpdated;
+    // 쿨타임 시작을 UI 슬롯들에게 알리는 이벤트
+    public event Action<int, float> OnItemCooldownStarted;
 
     private void Update()
     {
@@ -33,7 +40,8 @@ public class ConsumableQuickSlotManager : Singleton<ConsumableQuickSlotManager>
     {
         // 입력받은 슬롯의 아이템 저장
         ConsumableItem currentItem = quickSlots[slotIndex];
-        if (currentItem == null) return;
+        if (currentItem == null) return; 
+        if (IsItemOnCooldown(currentItem.ItemID)) return;
         // 아이템의 남은 개수에 대한 정보를 받아와 소지 개수가 없다면 리턴
         if (InventoryManager.Instance.GetConsumableItemAmount(currentItem.ItemID) <= 0)
         {
@@ -63,5 +71,26 @@ public class ConsumableQuickSlotManager : Singleton<ConsumableQuickSlotManager>
         quickSlots[slotIndex] = null;
         // UI 업데이트 하라는 이벤트
         OnQuickSlotUpdated?.Invoke();
+    }
+
+    public bool IsItemOnCooldown(int itemID)
+    {
+        return coolingDownItemIDs.Contains(itemID);
+    }
+    public void StartItemCooldown(int itemID, float duration)
+    {
+        if (coolingDownItemIDs.Contains(itemID) || duration <= 0f) return;
+        StartCoroutine(ItemCooldownCo(itemID, duration));
+    }
+    private IEnumerator ItemCooldownCo(int itemID, float duration)
+    {
+        coolingDownItemIDs.Add(itemID);
+
+        // 
+        OnItemCooldownStarted?.Invoke(itemID, duration);
+
+        yield return new WaitForSeconds(duration);
+
+        coolingDownItemIDs.Remove(itemID);
     }
 }
