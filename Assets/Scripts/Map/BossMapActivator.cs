952cb3907b9d547cbe686ed51fccfe000602cb23
ssuuji <sub9399@naver.com>
2026-07-01 11:03:07 +0900

    feat : 게임씬에 보스맵 추가
    - 플레이어가 보스맵에 들어갔을때 보스 활성화

---
+++ Assets/Scripts/Map/BossMapActivator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BossMap : MonoBehaviour
+{
+    [SerializeField] private GameObject boss;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        boss.SetActive(true); //보스활성화
+        gameObject.SetActive(false); //포탈 비활성화
+    }
+}
